449265d83ca9d1fb63214408aca9e56457f29193
JelR <resosemitojelani@gmail.com>
2026-03-03 02:38:40 -0300

    problem with last part of fetchUserInfo func

--- backend/api/services/fetchUserInfo.js
+++ backend/api/services/fetchUserInfo.js
@@ -1,5 +1,6 @@
 // file has to do with getting all types of info that is related to users
 import { prisma } from "#utils/prismaClient";
+// import {bcrypt} from bcrypt;
 
 // Example: Fetch all records from a table
 // Replace 'user' with your actual model name
@@ -30,20 +31,55 @@ async function exampleFunction(userId) {
 // * Actual funcs
 
 //!A way to get the data from the DB so you can COMPARE it with what the user send during LOGIN
+/**
+ * uses @param - userEmail, providedPassword  to get the user password info, and also validates if the login password is correct
+ * @returns an object?
+ */
 export async function validateUserLogin(userEmail, providedPassword) {
   const user = await prisma.users.findUnique({
     where: { email: userEmail },
-    select: { userId: true, saltedPassword: true },
+    select: { userId: true, saltedPassword: true, salt: true },
   });
 
   //validates if the user exist
   if (!user) return { success: false, message: "User Doesn't Exist" };
+
+  //Salts that password so we can actually validate itt with the stored password
+  const saltedProvidedPassword = await bcrypt.hash(providedPassword, user.salt);
+
   //validates the password
-  if (!user.saltedPassword === providedPassword)
+  if (!user.saltedPassword === saltedProvidedPassword)
     return { success: false, message: "Incorrect email or password" };
 
   //returns if sucessfull
   return { success: true, message: "Logged in", userId: user.userId };
 }
 
-//Getting just general user info like department, role, userId, and placeholder decrypt
+//get the info of a user from an object like {userId: something} to get userId, userRole, userDepartment
+/**
+ * ! seems a bit sketchy to hold the userId in this
+ * @param {*} userInfo
+ * @returns
+ */
+export async function fetchUserInfo(userInfo) {
+  const Info = await prisma.users.findUnique({
+    where: {
+      userId: userInfo.userId,
+    },
+    select: {
+      userId: true,
+      role: {
+        select: { roleName: true },
+      },
+      department: {
+        select: {
+          departmentName: true,
+        },
+      },
+    },
+  });
+  console.log(userInfo);
+  return userInfo;
+}
+
+fetchUserInfo({ userId: "f" });
